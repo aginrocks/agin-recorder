5ae6e56765fdc0153b3df148baf1d0730beeab02
Tymek <vonautymek@gmail.com>
2026-04-25 01:19:17 +0200

    added keep screen on flag

--- android/app/src/main/java/rocks/agin/recorder/MainActivity.kt
+++ android/app/src/main/java/rocks/agin/recorder/MainActivity.kt
@@ -1,6 +1,7 @@
 package rocks.agin.recorder
 
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,6 +14,7 @@ import rocks.agin.recorder.ui.theme.AginRecorderTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         enableEdgeToEdge()
 
         val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
