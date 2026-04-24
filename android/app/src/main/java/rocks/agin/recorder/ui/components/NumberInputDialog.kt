462e53ab6569bb21f4596db71375df35a317bd27
Tymek <vonautymek@gmail.com>
2026-04-25 01:59:00 +0200

    fix: closing dialog on `Done` button pres son keyboard

--- android/app/src/main/java/rocks/agin/recorder/ui/components/NumberInputDialog.kt
+++ android/app/src/main/java/rocks/agin/recorder/ui/components/NumberInputDialog.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.MaterialTheme
@@ -20,6 +21,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.input.TextFieldValue
@@ -60,7 +62,15 @@ fun NumberInputDialog(
                         }
                     },
                     label = { Text("Enter value") },
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Number,
+                        imeAction = ImeAction.Done
+                    ),
+                    keyboardActions = KeyboardActions(
+                        onDone = {
+                            textFieldValue.text.toIntOrNull()?.let { onConfirm(it) }
+                        }
+                    ),
                     modifier = Modifier
                         .fillMaxWidth()
                         .focusRequester(focusRequester),
